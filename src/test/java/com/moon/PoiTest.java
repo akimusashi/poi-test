ac3abfb3bd602d3c845336721d723ede4f96e314
akimusashi <akimusashi@outlook.com>
2018-03-26 16:53:27 +0800

    update

--- src/test/java/com/moon/PoiTest.java
+++ src/test/java/com/moon/PoiTest.java
@@ -4,6 +4,8 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +21,8 @@ public class PoiTest {
         bodyList.add(new MyBook("道德经", "100.00"));
         bodyList.add(new MyBook("孙子兵法", "10.00"));
         HSSFWorkbook workbook = PoiDemo.expExcel("书籍管理", headList, colNames, bodyList);
-        PoiDemo.outFile(workbook, "C:\\Users\\User\\Desktop\\book.xls");
+        File desktopDir = FileSystemView.getFileSystemView().getHomeDirectory();
+        String desktopPath = desktopDir.getAbsolutePath();
+        PoiDemo.outFile(workbook, desktopPath + "book.xls");
     }
 }
